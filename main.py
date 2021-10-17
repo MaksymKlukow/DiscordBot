897946c448fd9c04f0f36bd7943772b398e70d45
gfoxi <piksoxd@gmail.com>
2021-10-17 21:58:44 +0200

    Safe connecting with token

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-import discord
-import requests
-import json
-import time
-import random
-import asyncio
+import credentials
 from replit import db
 
-TOKEN = "ODk5MzU3NTQ4NjIzMzA2Nzcy.YWxl_A.yk02UEHqTtZorUYcTvg_6PbooJc"
+import discord
+
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print("Pomyslnie wlaczono bota")
-
 
 @client.event
 async def on_message(message):
@@ -22,4 +17,4 @@ async def on_message(message):
     if message.content.startswith('ping'):
         await message.channel.send("pong")
 
-client.run(TOKEN)
+client.run(credentials.TOKEN)
