26f2c4434d050897f66b8e7b236836d9d6d60cb5
gfoxi <piksoxd@gmail.com>
2021-10-17 22:12:23 +0200

    Trying to do random

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from replit import db
 import discord
 
 client = discord.Client()
+WORDS = ("python", "jumble", "easy", "difficult", "answer",  "xylophone")
 
 @client.event
 async def on_ready():
@@ -18,5 +19,8 @@ async def on_message(message):
         await message.channel.send("pong")
     if message.content.startswith('pong'):
         await message.channel.send("ping test")
+
+    if message.content.startswith('ruletka'):
+        await message.channel.send(random.choice(WORDS))
 
 client.run(credentials.TOKEN) #credentials.py for saver usage
